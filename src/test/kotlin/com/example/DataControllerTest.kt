e31312e9c05266b62000d8cd51551fc412c6c585
Alex Arana <alex.arana@team.telstra.com>
2023-09-13 10:20:44 +1000

    Added streaming tests.

--- src/test/kotlin/com/example/DataControllerTest.kt
+++ src/test/kotlin/com/example/DataControllerTest.kt
@@ -3,6 +3,7 @@ package com.example
 import io.kotest.core.spec.style.ExpectSpec
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
+import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -33,9 +34,27 @@ internal class DataControllerTest(private val webTestClient: WebTestClient) : Ex
                 .isOk
         }
 
+        expect("should return HTTP 200 when streaming Data") {
+            webTestClient.get()
+                .uri("/data")
+                .accept(MediaType.APPLICATION_NDJSON)
+                .exchange()
+                .expectStatus()
+                .isOk
+        }
+
         expect("should return HTTP 400 when requesting Data with error") {
             webTestClient.get()
                 .uri("/data-error")
+                .exchange()
+                .expectStatus()
+                .isBadRequest
+        }
+
+        expect("should return HTTP 400 when streaming Data with error") {
+            webTestClient.get()
+                .uri("/data-error")
+                .accept(MediaType.APPLICATION_NDJSON)
                 .exchange()
                 .expectStatus()
                 .isBadRequest
